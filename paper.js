1afbb0bf21377e6500854122f77162068dd161ed
DarshbirSingh <70876220+DarshbirSingh@users.noreply.github.com>
2020-10-29 10:00:25 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -3,7 +3,7 @@ class Paper {
     var options = { isStatic:false,restitution:0.3, friction:0.5, density:1.2 } 
     this.body = Bodies.circle(x, y, 70, options); 
     this.radius = 70; 
-    this.image=loadImage("Images/paper.png")
+    this.image=loadImage("paper.png")
     World.add(world, this.body); }
     display(){
     var pos=this.body.position
